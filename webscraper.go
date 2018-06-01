cd2f608ff5dc35b00dbe43cabf5076d76520e6d3
Timo Müller <work@mtimo.de>
2018-06-02 01:00:09 +0200

    concurrency

--- webscraper.go
+++ webscraper.go
@@ -9,7 +9,10 @@ import (
 	"strings"
 	"io/ioutil"
 	"io"
+	"sync"
 )
+
+var path = "./responses/"
 
 func main() {
 	//https://www.zerodayinitiative.com/advisories/ZDI-17-001/
@@ -19,50 +22,90 @@ func main() {
 		os.Exit(1)
 	}
 	url := os.Args[1]
+	//url = "127.0.0.1/%d"
 	// Create directory into which we will save the responses
-	path := "./responses/"
+
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		os.Mkdir(path, os.ModePerm)
 	}
-	urlEdited := ""
-	missingCount := 0
-	responseDataStringOld := ""
-	for i := 1010; i < 1020 && missingCount < 2; i++ { // TODO multi threaded
-		urlEdited = fmt.Sprintf(url, i)
+	var wg sync.WaitGroup
 
-		response, err := http.Get(urlEdited)
-		if response.Status != "200" {
-			fmt.Println("Unknown Status: " + response.Status + " " + urlEdited)
-		} else {
+	queryResponseChannel := make(chan bool, 3)
+	//for i := 1010; i < 1015 && missingCount < 2; i++ { // TODO multi threaded
+	for i := 1010; i < 1015; i++ { // TODO multi threaded
+		//missingCount := 0
+		wg.Add(1)
+		go querySite(queryResponseChannel, &wg, url, i)
+	}
+	//<-queryResponseChannel
+	wg.Wait()
+	close(queryResponseChannel)
+	
+}
 
-			if err != nil {
-				log.Fatal(err)
-			} else {
-				responseData, err := ioutil.ReadAll(response.Body)
-				responseDataString := string(responseData)
-				if err != nil {
-					log.Fatal(err)
-				}
-				if strings.Contains(responseDataString, "advisories-details") && !(responseDataStringOld == responseDataString) { // TODO make real xml parser and only write important tags
-					fmt.Println("Writing: " + urlEdited)
-					missingCount = 0
-					index := strings.LastIndex(urlEdited[:len(urlEdited)-1], "/")
-					responseIndex := urlEdited[index:]
-					responseIndex = strings.Replace(responseIndex, "/", "", -1) //-1 unlimited replacements
-					f, err := os.Create(path + responseIndex + ".html")
-					io.WriteString(f, responseDataString)
-					if err != nil {
-						log.Fatal(err)
-					}
-					f.Close()
-				} else {
-					fmt.Println("Duplicate/not expected: " + urlEdited)
-					missingCount++
-				}
-				responseDataStringOld = responseDataString
-			}
-			response.Body.Close()
+func querySite(queryResponseChannel chan<- bool, wg *sync.WaitGroup, queryUrl string, i int) {
+	defer wg.Done()
+	urlEdited := fmt.Sprintf(queryUrl, i)
+	fmt.Println("Sende Request für: " + urlEdited)
+	response, err := http.Get(urlEdited)
+	responseDataString := ""
+	if response == nil {
+		fmt.Println("Response nil, error")
+		return
+	}
+	if response.Status != "200 OK" {
+		fmt.Println("Unknown Status: " + response.Status + " " + urlEdited)
+	} else {
+		responseData, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
 		}
+		responseDataString = string(responseData)
+
+		//if strings.Contains(responseDataString, "advisories-details") && !(responseDataStringOld == responseDataString) { // TODO make real xml parser and only write important tags
+		if strings.Contains(responseDataString, "advisories-details") { // TODO make real xml parser and only write important tags
+			fmt.Println("Writing: " + urlEdited)
+
+		} else {
+			fmt.Println("Duplicate/not expected: " + urlEdited)
+
+		}
+		//responseDataStringOld = responseDataString
+
+		response.Body.Close()
 	}
 
+	if err != nil {
+		log.Fatal(err)
+	}
+	go writeToDisk(responseDataString, urlEdited)
+	//queryResponseChannel <- true
+}
+
+func writeToDiskGo(writeChannel chan<- bool, responseBodyString, queryUrlEdited string) {
+	fmt.Println("Schreibe Datei für; " + queryUrlEdited)
+	index := strings.LastIndex(queryUrlEdited[:len(queryUrlEdited)-1], "/")
+	responseIndex := queryUrlEdited[index:]
+	responseIndex = strings.Replace(responseIndex, "/", "", -1) //-1 unlimited replacements
+
+	f, err := os.Create(path + responseIndex + ".html")
+	defer f.Close()
+	io.WriteString(f, responseBodyString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	writeChannel <- true
+}
+func writeToDisk(responseBodyString, queryUrlEdited string) {
+	fmt.Println("Schreibe Datei für; " + queryUrlEdited)
+	index := strings.LastIndex(queryUrlEdited[:len(queryUrlEdited)-1], "/")
+	responseIndex := queryUrlEdited[index:]
+	responseIndex = strings.Replace(responseIndex, "/", "", -1) //-1 unlimited replacements
+
+	f, err := os.Create(path + responseIndex + ".html")
+	defer f.Close()
+	io.WriteString(f, responseBodyString)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
